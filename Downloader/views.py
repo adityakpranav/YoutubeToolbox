de4ff83dda14f84569f84ab005e0e3caa2d98aae
Night Fury <adityakpranav@gmail.com>
2021-05-07 20:58:54 +0530

    addon changes for VACCBot app

--- Downloader/views.py
+++ Downloader/views.py
@@ -102,6 +102,12 @@ def temp_fetchVaccCenter_redirect(jsonrequest):
 
         headers = {
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Connection":	"keep-alive",
+            "DNT":	"1",
+            "TE": "Trailers",
+            "Upgrade-Insecure-Requests": "1",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0"}
 
         url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=%s&date=%s" % (
