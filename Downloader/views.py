b32966b08276052b97575c1f4c6149dcbc78220f
Night Fury <adityakpranav@gmail.com>
2021-05-07 20:44:43 +0530

    addon changes for VACCBot app

--- Downloader/views.py
+++ Downloader/views.py
@@ -102,21 +102,21 @@ def temp_fetchVaccCenter_redirect(jsonrequest):
 
         headers = {
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Accept-Language": "en-US,en;q=0.5",
-            "Connection":	"keep-alive",
-            "DNT":	"1",
-            "TE": "Trailers",
-            "Upgrade-Insecure-Requests": "1",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0"}
 
-        url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=%s&date=%s" % (
+        url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=%s&date=%s" % (
             ourid, datetime.today().strftime("%d-%m-%Y"))
+        #print("url: ", url)
 
         req = request.Request(url, None, headers)
-        data = json.loads(request.urlopen(req).read())
+        #data = json.loads(request.urlopen(req).read())
 
+        with request.urlopen(req) as response:
+
+            # .decode(response.headers.get_content_charset())
+            data = response.read().decode("utf-8")
         # return JsonHttpResponse(data)
+        print("data::", data)
         return(data)
 
     else:
--- YTDPython/settings.py
+++ YTDPython/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fc7#vvg#nft!977hqr66j19hl8o8on&%!$m$x@9x!#(-m1@551
 #SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1',
                  'youtubetoolbox.herokuapp.com']
