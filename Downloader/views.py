ff708900dc48a8f2fd039ff4b08f3c7b02eecd03
Night Fury <adityakpranav@gmail.com>
2021-05-07 15:02:25 +0530

    addon changes for VACCBot app

--- Downloader/views.py
+++ Downloader/views.py
@@ -17,7 +17,8 @@ from datetime import datetime
 
 def index(request):
     # print(list(get_files(StaticFilesStorage(), location='static')))
-    print("[Log ] index-temp_fetchVaccCenter_redirect: ", temp_fetchVaccCenter_redirect({"postData": "645"})
+    print("[Log ] index-temp_fetchVaccCenter_redirect: ",
+          temp_fetchVaccCenter_redirect({"postData": "645"}))
     return render(request, 'Downloader/index.html')
 
 
@@ -25,32 +26,32 @@ def fetchVideo(request):
 
     if request.is_ajax() and request.method == 'POST':
 
-        ourid=request.POST["postData"]
+        ourid = request.POST["postData"]
         return JsonHttpResponse(fetchVideoURL(ourid))
 
     else:
 
-        html='<p>This is not ajax</p>'
+        html = '<p>This is not ajax</p>'
         return JsonResponse({{'data': {'msg': "Error"}}})
 
 
 def JsonHttpResponse(jsonData):
 
-    response_data={}
-    response_data['result']='success'
-    response_data['data']=jsonData
-    response_data["Content-Type"]="application/octet-stream"
+    response_data = {}
+    response_data['result'] = 'success'
+    response_data['data'] = jsonData
+    response_data["Content-Type"] = "application/octet-stream"
     # 'filename="download.mp4"'
-    response_data['Content-Disposition']='attachment; filename="download.mp4"'
+    response_data['Content-Disposition'] = 'attachment; filename="download.mp4"'
 
     return HttpResponse(json.dumps(response_data), content_type="application/json")
 
 
 def yt_details(url):
-    ytb=YouTube(url)
-    data={'data': {
-            'msg': "OK",
-            'title': ytb.title,
+    ytb = YouTube(url)
+    data = {'data': {
+        'msg': "OK",
+        'title': ytb.title,
             'views': ytb.length,
             'length': ytb.length,
             'description': ytb.description,
@@ -61,25 +62,25 @@ def yt_details(url):
 
 
 def fetchVideoURL(url):
-    video=pafy.new(url)
+    video = pafy.new(url)
 
-    videostreams=video.videostreams
-    audiostreams=video.audiostreams
+    videostreams = video.videostreams
+    audiostreams = video.audiostreams
 
-    video_data_name_url_dict={}
-    audio_data_name_url_dict={}
+    video_data_name_url_dict = {}
+    audio_data_name_url_dict = {}
 
     # video urls
     for i in range(len(videostreams)):
         video_data_name_url_dict[str(
-            videostreams[i]).split("@")[-1]]=videostreams[i].url_https
+            videostreams[i]).split("@")[-1]] = videostreams[i].url_https
 
     # audio urls
     for i in range(len(audiostreams)):
         audio_data_name_url_dict[str(
-            audiostreams[i]).split("@")[-1]]=audiostreams[i].url_https
+            audiostreams[i]).split("@")[-1]] = audiostreams[i].url_https
 
-    payload={"title": video.title,
+    payload = {"title": video.title,
                "time": video.duration,
                "author": video.author,
                "Vdata": video_data_name_url_dict,
@@ -96,9 +97,10 @@ def temp_fetchVaccCenter_redirect(jsonrequest):
     # if request.method == 'POST':
     if jsonrequest != {}:
 
-        ourid=request.POST["postData"]
+        #ourid = request.POST["postData"]
+        ourid = jsonrequest["postData"]
 
-        headers={
+        headers = {
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
             "Accept-Encoding": "gzip, deflate, br",
             "Accept-Language": "en-US,en;q=0.5",
@@ -108,17 +110,17 @@ def temp_fetchVaccCenter_redirect(jsonrequest):
             "Upgrade-Insecure-Requests": "1",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0"}
 
-        url="https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=%s&date=%s" % (
+        url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=%s&date=%s" % (
             ourid, datetime.today().strftime("%d-%m-%Y"))
 
-        req=request.Request(url, None, headers)
-        data=json.loads(request.urlopen(req).read())
+        req = request.Request(url, None, headers)
+        data = json.loads(request.urlopen(req).read())
 
         return JsonHttpResponse(data)
 
     else:
 
-        html='<p>This is not ajax</p>'
+        html = '<p>This is not ajax</p>'
         return JsonResponse({{'data': {'msg': "Error"}}})
 
 
